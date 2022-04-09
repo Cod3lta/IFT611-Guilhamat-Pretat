240cf46345ab462debfa016e8fc2ad47654bade1
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-04-08 20:55:35 -0400

    add functions for interpolation and server client reconciliation

--- IFT611_Guilhamat_Pretat/character/Player.gd
+++ IFT611_Guilhamat_Pretat/character/Player.gd
@@ -13,11 +13,23 @@ var input_x: float # The player's key input
 puppet var puppet_velocity = Vector2.ZERO
 puppet var puppet_position = Vector2.ZERO
 
+
+master var remote_movement = Vector2()
+puppet var remote_transform = Transform()
+puppet var remote_vel = Vector2()
+
+#client server reconciliation
+export var csr = true # Client Server Reconciliation
+puppet var ack = 0 # Last movement acknowledged
+var old_movement = Vector2()
+var time = 0
+
+
 signal die(player)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_network_master(1)
 
 func init(color_id: int):
 	$Sprite.set_texture(load("res://character/player-" + str(color_id) + ".png"))
@@ -30,7 +42,7 @@ func _process(delta):
 	
 	jump()
 	
-	multiplayer_movements()
+	multiplayer_movements(delta)
 	apply_movements()
 	
 	animate()
@@ -54,12 +66,13 @@ func get_gravity():
 	if !is_on_floor() and velocity.y < MAX_SPEED.y:
 		velocity.y += GRAVITY
 
-func multiplayer_movements():
+func multiplayer_movements(delta):
 	if is_network_master():
 		# TODO : don't send the position and velocity in every frame
 		rset_unreliable("puppet_velocity", velocity) # used for the puppet's animations
 		rset_unreliable("puppet_position", position)
 	else:
+		time += delta
 		position = puppet_position
 		velocity = puppet_velocity
 
@@ -84,3 +97,21 @@ func animate():
 
 func _on_DeathDetector_body_entered(body):
 	emit_signal("die", self)
+
+
+func move_with_reconciliation(delta):
+	var old_transform = transform
+	transform = remote_transform
+	var vel = remote_vel
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	interpolate(old_transform)
+
+func interpolate(old_transform):
+	var timeBetween # TODO add the time between the two last received 
+	transform.origin = old_transform.origin.linear_interpolate(transform.origin,timeBetween)
+
+puppet func update_state(t, velocity, ack):
+	self.remote_transform = t
+	self.remote_vel = velocity
+	self.ack = ack
